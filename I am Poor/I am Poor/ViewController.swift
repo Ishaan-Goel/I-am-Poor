0c3421fb5040758a2f9f10a4452c17b253a1150e
Ishaan-Goel <139787489+Ishaan-Goel@users.noreply.github.com>
2023-12-31 15:38:08 -0800

    Add files via upload

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,16 @@
+//  ViewController.swift
+//  I am Poor
+//  Created by Ishaan Goel on 8/26/23.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
